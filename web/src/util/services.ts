5b7232fc0dba139a80c5bf8e81d60ed3c45411aa
Giovanni Schenato <gischenato@gmail.com>
2023-05-08 16:34:19 -0300

    baseUrl

---
+++ web/src/util/services.ts
@@ -0,0 +1,8 @@
+export const baseUrl = 'http://localhost:5000'
+
+export const axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+}
