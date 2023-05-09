ff8c8e840e0255f7ad34e252e335bbb7356a1dc9
Giovanni Schenato <gischenato@gmail.com>
2023-05-09 16:38:34 -0300

    configuring path aliases with vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@util': path.resolve(__dirname, './src/util'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
