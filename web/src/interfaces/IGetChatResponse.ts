a50dd238ba9d40bc78677909a4f8d217d67135c7
Giovanni Schenato <gischenato@gmail.com>
2023-05-10 15:19:20 -0300

    typing getChat response

---
+++ web/src/interfaces/IGetChatResponse.ts
@@ -0,0 +1,5 @@
+import IChat from "./IChat";
+
+type IGetChatResponse = IChat[] 
+
+export default IGetChatResponse;
--- web/src/util/api/chats/getChat.ts
+++ web/src/util/api/chats/getChat.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { baseUrl } from "@util/services";
+import IGetChatResponse from "@interfaces/IGetChatResponse";
 
 export const getChat = async (chatId: string) => {
-    const response = await axios.get(`${baseUrl}/chat/${chatId}`);
+    const response = await axios.get<IGetChatResponse>(`${baseUrl}/chat/${chatId}`);
     return response.data;
 };
