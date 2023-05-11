01a6bd69f5053872d8a9062eed62a286999c63bf
Giovanni Schenato <gischenato@gmail.com>
2023-05-11 16:37:56 -0300

    chat page

--- web/src/pages/Chat/index.tsx
+++ web/src/pages/Chat/index.tsx
@@ -10,6 +10,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import Message from './Message'
+import Input from './Input'
 
 export default function Chat() {
   const { chatId, userId } = useParams()
@@ -20,21 +21,27 @@ export default function Chat() {
     queryKey: ['messages', chatId],
     queryFn: () => getMessages(chatId),
   })
+
+  const sendMessage = (text: string) => {
+    console.log('sending message', text)
+  }
   
   
   return (
     <Container>
-      <TitleText>Chat with </TitleText>
-      <RegularText>Chat  id: {chatId}</RegularText>
-      <RegularText>My    id: {myUser?._id}</RegularText>
-      <RegularText>Other id: {userId}</RegularText>
-      {/* <RegularText>Other id: {._id}</RegularText> */}
-      <br/>
-      <br/>
-      <RegularText>Messages:</RegularText>
-      { messagesQuery.status === 'loading' && <p>Loading...</p>}
-      { messagesQuery.status === 'error' && <p>Error fetching messages</p>}
-      { messagesQuery.status === 'success' && messagesQuery.data.map(msg => <Message key={msg._id} message={msg}/>)}
+      <TitleText>Chat with {otherUser?.name}</TitleText>
+      <ChatContainer>
+        <MessagesContainer>
+        { messagesQuery.status === 'loading' && <p>Loading...</p>}
+        { messagesQuery.status === 'error' && <p>Error fetching messages</p>}
+        { messagesQuery.status === 'success' && messagesQuery.data.map(msg => 
+            <Message key={msg._id} message={msg} isMine={msg.senderId===myUser?._id}/>
+        )}
+        </MessagesContainer>
+        <div id='inp'>
+          <Input onSubmit={sendMessage}/>
+        </div>
+      </ChatContainer>
     </Container>
   )
 }
@@ -43,5 +50,57 @@ export default function Chat() {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  flex: 1;
   line-height: 1.3;
+  position: relative;
+
+  background-color: violet;
+  padding: 2rem;
+  overflow: hidden;
+  box-sizing: border-box;
+
+  max-height: 100%;
+
+  overflow-y: auto;
+  
+  `
+
+const ChatContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 100%;
+  position: relative;
+  padding: 1rem;
+  height: 100%;
+  overflow: hidden;
+  justify-content: flex-end;
+  
+  #inp {
+    margin-top: 8px;
+  }
+  `
+
+const MessagesContainer = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column-reverse;
+  overflow-y: auto;
+
+  height: 100%;
+
+  gap: 8px;
+
+  ::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #555;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #a3a3a3;
+    border-radius: 8px;
+  }
 `
