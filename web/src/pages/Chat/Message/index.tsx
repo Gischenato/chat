46731f5f4128ad868421d0fc1d678e6ee77115b5
Giovanni Schenato <gischenato@gmail.com>
2023-05-11 16:37:37 -0300

    Message component

--- web/src/pages/Chat/Message/index.tsx
+++ web/src/pages/Chat/Message/index.tsx
@@ -1,21 +1,31 @@
 import IMessage from '@interfaces/IMessage'
 import { RegularText } from '@styles/typography'
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 interface MessageProps {
   message: IMessage
+  isMine?: boolean
 }
 
-export default function Message({ message }: MessageProps ) {
+export default function Message({ message, isMine }: MessageProps ) {
   return (
-    <Container>
+    <Container isMine={isMine}>
       <RegularText font='title' size={'l'}>{message.text}</RegularText>
     </Container>
   )
 }
 
 
-const Container = styled.div`
-
+const Container = styled.div<{ isMine?: boolean }>`
+  display: flex;
+  ${({ isMine }) => css`
+    > p {
+      background-color: ${isMine ? 'aliceblue' : 'aquamarine'};
+      max-width: 50%;
+      padding: 8px;
+      border-radius: 8px;
+    }
+    justify-content: ${isMine ? 'flex-end' : 'flex-start'};
+  `}
 `
